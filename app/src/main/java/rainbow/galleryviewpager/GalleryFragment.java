5e9a4e61757dd2a2a7d0a9b8981c36a222de1529
夏阳(寿险总部销售平台开发部E行销开发团队新产品技术组) <xiayang075@pingan.com.cn>
2017-10-23 11:22:16 +0800

    通过View复用和ViewHolder复用机制优化ViewPager性能

--- app/src/main/java/rainbow/galleryviewpager/GalleryFragment.java
+++ app/src/main/java/rainbow/galleryviewpager/GalleryFragment.java
@@ -9,6 +9,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 import rainbow.library.GalleryViewPager;
 
 /**
@@ -20,9 +24,11 @@ public class GalleryFragment  extends Fragment {
       R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3, R.mipmap.pic4, R.mipmap.pic5
   };
 
+  private Queue<View> viewPool = new LinkedList<>(); //View池
+
   @Nullable @Override public View onCreateView(final LayoutInflater inflater, ViewGroup container,
       Bundle savedInstanceState) {
-    GalleryViewPager viewPager =
+    final GalleryViewPager viewPager =
         (GalleryViewPager) inflater.inflate(R.layout.fragmet_gallery, container, false);
     viewPager.setAdapter(new PagerAdapter() {
       @Override public int getCount() {
@@ -34,15 +40,27 @@ public class GalleryFragment  extends Fragment {
       }
 
       @Override public Object instantiateItem(ViewGroup container, int position) {
-        View view = inflater.inflate(R.layout.gallery_item, container, false);
-        ((TextView) view.findViewById(R.id.title)).setText("Pager " + position);
-        ((ImageView) view.findViewById(R.id.imageview)).setImageResource(PIC_RES[position]);
+        View view;
+        ViewHolder viewHolder;
+        if(viewPool.size() > 0){
+          view = viewPool.poll();
+          viewHolder = (ViewHolder) view.getTag();
+        }else{
+          view = inflater.inflate(R.layout.gallery_item, container, false);
+          viewHolder = new ViewHolder();
+          viewHolder.textView = ((TextView) view.findViewById(R.id.title));
+          viewHolder.imageView = ((ImageView) view.findViewById(R.id.imageview));
+          view.setTag(viewHolder);
+        }
+        viewHolder.textView.setText("Pager " + position);
+        viewHolder.imageView.setImageResource(PIC_RES[position]);
         container.addView(view);
         return view;
       }
 
       @Override public void destroyItem(ViewGroup container, int position, Object object) {
         container.removeView((View) object);
+        viewPool.offer((View) object);
       }
 
       @Override public float getPageWidth(int position) {
@@ -52,4 +70,9 @@ public class GalleryFragment  extends Fragment {
     viewPager.setNarrowFactor(0.9f);
     return viewPager;
   }
+
+   class ViewHolder{
+     TextView  textView;
+     ImageView imageView;
+   }
 }
